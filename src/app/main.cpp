28435380c2b9198e2ac7855513a3ec7760863c5d
Steffen André Langnes <steffenl.sw@gmail.com>
2022-05-05 05:44:44 +0900

    Convert project to use gtk directly instead of gtkmm

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,20 +1,22 @@
 #include "core/meta.hpp"
-#include <gtkmm.h>
+#include <gtk/gtk.h>
 
-class MainWindow : public Gtk::Window {
-public:
-    MainWindow() {
-        set_title("Example");
-        set_default_size(600, 400);
-        m_label.set_text("Core version: " + example::core::getVersion());
-        set_child(m_label);
-    }
+void onActivate(GtkApplication* app) {
+    auto window{gtk_application_window_new(app)};
+    gtk_window_set_title(GTK_WINDOW(window), "Example");
+    gtk_window_set_default_size(GTK_WINDOW(window), 600, 400);
 
-private:
-    Gtk::Label m_label;
-};
+    std::string labelText{"Core version: " + example::core::getVersion()};
+    auto label{gtk_label_new_with_mnemonic(nullptr)};
+    gtk_label_set_text(GTK_LABEL(label), labelText.c_str());
+    gtk_window_set_child(GTK_WINDOW(window), label);
+
+    gtk_window_present(GTK_WINDOW(window));
+
+}
 
 int main(int argc, char* argv[]) {
-    auto app{Gtk::Application::create("es.langn.examples.gtkmm")};
-    return app->make_window_and_run<MainWindow>(argc, argv);
+    auto app{gtk_application_new("es.langn.examples.GtkExampleApp", G_APPLICATION_FLAGS_NONE)};
+    g_signal_connect(app, "activate", G_CALLBACK(onActivate), nullptr);
+    return g_application_run(G_APPLICATION(app), argc, argv);
 }
