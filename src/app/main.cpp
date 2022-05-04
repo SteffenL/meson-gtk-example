555358ccff1b3706dba8ef8261d360061de47b85
Steffen André Langnes <steffenl.sw@gmail.com>
2022-05-04 17:01:21 +0900

    Display core version in GUI

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,17 +1,20 @@
 #include "core/meta.hpp"
 #include <gtkmm.h>
-#include <iostream>
 
 class MainWindow : public Gtk::Window {
 public:
     MainWindow() {
         set_title("Example");
         set_default_size(600, 400);
+        m_label.set_text("Core version: " + example::core::getVersion());
+        set_child(m_label);
     }
+
+private:
+    Gtk::Label m_label;
 };
 
 int main(int argc, char* argv[]) {
-    std::cout << "Core version: " << example::core::getVersion() << "\n";
     auto app{Gtk::Application::create("es.langn.examples.gtkmm")};
     return app->make_window_and_run<MainWindow>(argc, argv);
 }
