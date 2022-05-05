58fd61a598e0cfeb8200f7e82ca13cfc6c663f25
Steffen André Langnes <steffenl.sw@gmail.com>
2022-05-05 12:00:47 +0900

    Load UI from XML resource

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -2,21 +2,24 @@
 #include <gtk/gtk.h>
 
 void onActivate(GtkApplication* app) {
-    auto window{gtk_application_window_new(app)};
-    gtk_window_set_title(GTK_WINDOW(window), "Example");
-    gtk_window_set_default_size(GTK_WINDOW(window), 600, 400);
+    auto builder{gtk_builder_new()};
+    gtk_builder_add_from_resource(builder, "/es/langn/examples/GtkExampleApp/main.ui", nullptr);
+
+    auto window{gtk_builder_get_object(builder, "main_window")};
+    gtk_window_set_application(GTK_WINDOW(window), app);
 
     std::string labelText{"Core version: " + example::core::getVersion()};
-    auto label{gtk_label_new_with_mnemonic(nullptr)};
+    auto label{gtk_builder_get_object(builder, "label")};
     gtk_label_set_text(GTK_LABEL(label), labelText.c_str());
-    gtk_window_set_child(GTK_WINDOW(window), label);
 
-    gtk_window_present(GTK_WINDOW(window));
-
+    gtk_widget_show(GTK_WIDGET(window));
+    g_object_unref(builder);
 }
 
 int main(int argc, char* argv[]) {
     auto app{gtk_application_new("es.langn.examples.GtkExampleApp", G_APPLICATION_FLAGS_NONE)};
     g_signal_connect(app, "activate", G_CALLBACK(onActivate), nullptr);
-    return g_application_run(G_APPLICATION(app), argc, argv);
+    auto status{g_application_run(G_APPLICATION(app), argc, argv)};
+    g_object_unref(app);
+    return status;
 }
