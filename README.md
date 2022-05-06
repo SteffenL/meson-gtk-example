caf1af5f7167d8001d6d61606f31f82877bf02d4
Steffen André Langnes <steffenl.sw@gmail.com>
2022-05-06 09:43:05 +0900

    wip

---
+++ README.md
@@ -0,0 +1,15 @@
+# Example App Using GTK and Meson
+
+## Compilation
+
+### Native
+
+```
+./scripts/build.sh
+```
+
+### Linux -> Windows
+
+```
+./scripts/build.sh --cross-file profiles/cross/mingw.ini
+```
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 set -e
 meson subprojects download sigc++-3.0 graphene gtk4
-meson setup --native-file profiles/default.ini builddir
+meson setup --native-file profiles/default.ini "${@}" builddir
 meson compile -C builddir app
