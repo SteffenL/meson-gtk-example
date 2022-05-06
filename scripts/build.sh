7825c9e5066f21a51936adc4c9265d330961b878
Steffen André Langnes <steffenl.sw@gmail.com>
2022-05-06 10:11:09 +0900

    wip

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,27 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -e
-meson subprojects download sigc++-3.0 graphene gtk4
-meson setup --native-file profiles/default.ini "${@}" builddir
-meson compile -C builddir app
+#meson subprojects download sigc++-3.0 graphene gtk4
+
+compile_type=${1}
+cross_file=${2}
+declare -a setup_args
+
+if [[ -z "${compile_type}" ]]; then
+    compile_type=native
+fi
+
+if [[ "${compile_type}" = "cross" && -z "${cross_file}" ]]; then
+    echo "Please specify a cross file."
+    exit 1
+fi
+
+setup_args+=("--${compile_type}-file")
+setup_args+=("profiles/default.ini")
+
+if [[ "${compile_type}" = "cross" ]]; then
+    setup_args+=("--${compile_type}-file")
+    setup_args+=("profiles/cross/${cross_file}.ini")
+fi
+
+meson setup builddir "${setup_args[@]}"
+#meson compile -C builddir app
