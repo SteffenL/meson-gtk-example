0be79d499d0d3571d5cc25539b8aff9f598f4f77
Steffen André Langnes <steffenl.sw@gmail.com>
2022-05-07 13:40:33 +0900

    Working state on Windows

--- include/core/exports.hpp
+++ include/core/exports.hpp
@@ -1,7 +1,13 @@
 #pragma once
 
-#ifdef BUILDING_CORE
-#  define CORE_API __declspec(dllexport)
-#else
-#  define CORE_API __declspec(dllimport)
+#ifndef CORE_API
+#  ifdef CORE_STATIC
+#    define CORE_API
+#  else
+#    ifdef BUILDING_CORE
+#      define CORE_API __declspec(dllexport)
+#    else
+#      define CORE_API __declspec(dllimport)
+#    endif
+#  endif
 #endif
