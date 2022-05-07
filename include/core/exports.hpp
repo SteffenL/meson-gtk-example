b94436a93540f7a4f6609eb89334c7ea5e15d6ce
Steffen André Langnes <steffenl.sw@gmail.com>
2022-05-07 18:45:30 +0900

    wip

--- include/core/exports.hpp
+++ include/core/exports.hpp
@@ -4,10 +4,14 @@
 #  ifdef CORE_STATIC
 #    define CORE_API
 #  else
-#    ifdef BUILDING_CORE
-#      define CORE_API __declspec(dllexport)
-#    else
-#      define CORE_API __declspec(dllimport)
+#    ifdef WIN32
+#      ifdef BUILDING_CORE
+#        define CORE_API __declspec(dllexport)
+#      else
+#        define CORE_API __declspec(dllimport)
+#      endif
 #    endif
+#  else
+#    define CORE_API __attribute__((visibility("default")))
 #  endif
 #endif
